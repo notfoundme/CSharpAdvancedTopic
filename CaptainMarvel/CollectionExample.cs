2f4510947756b3f3873e4772ce134f31f0fb952f
notfoundme <garimabhattarai059@gmail.com>
2024-01-24 08:55:45 +0545

    ArrayList and Hashtable completed

---
+++ CaptainMarvel/CollectionExample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace CaptainMarvel
+{
+    class CollectionExample
+    {
+        public void arrayListExample() {
+            ArrayList a1 = new ArrayList();
+            a1.Add(5);
+            a1.Add(9);
+            a1.Add(12);
+            a1.AddRange(new int[2] { 15, 23 });
+
+
+
+            Console.WriteLine(a1[2]);
+            Console.WriteLine("=========================");
+            foreach (var element in a1) {
+                Console.WriteLine(element);
+            }
+            
+        }
+
+
+        public void hashTableExample() {
+            Hashtable h1 = new Hashtable();
+            h1.Add("name", "sulav");
+            h1.Add("age", new int[5] {  2, 3, 5, 7, 9 });
+
+            Console.WriteLine(h1["name"]);
+            Console.WriteLine(((int[])h1["age"])[2]);
+        }
+    }
+}
--- CaptainMarvel/Program.cs
+++ CaptainMarvel/Program.cs
@@ -7,21 +7,9 @@ namespace CaptainMarvel
     {
         static void Main(string[] args)
         {
-            DelegateExample delE = new DelegateExample();
-
-            // Delegate are function pointer.
-            // Here, op1 is a delegate that points to sum method of delE
-            DelegateExample.op op1 = new DelegateExample.op(delE.sum);
-            op1 += delE.diff;
-            op1 += delE.diff;
-            op1 += delE.sum;
-            op1 -= delE.diff;
-            // so whenever op1 is called, sum is invoked.
-            op1(10, 5);
-
-
-
-
+            CollectionExample c = new CollectionExample();
+            c.arrayListExample();
+            c.hashTableExample();
         }
     }
 }
